418ad0b0e81dfed871b243e358478206d043180f
YY <yongfan98@gmail.com>
2023-03-22 22:03:15 +0800

    fix reactive of CodeBlock

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -2,11 +2,17 @@ import hl from 'highlight.js'
 import { createResource, createSignal } from 'solid-js'
 
 export function CodeBlock(props: { url: string }) {
-  const [res] = createResource(async () => {
-    const resp = await fetch(props.url)
-    return resp.text()
-  })
-  const render = ()=>{
+
+  const [res] = createResource(
+    // 注意不能用props.url,来替代()=>props.url,
+    // 也不能将props.url 直接写到下面的async 函数里
+    // 否则将失去reactive
+    () => props.url,
+    async (url: string) => {
+      const resp = await fetch(url)
+      return resp.text()
+    })
+  const render = () => {
     const el = <pre><code>{res()}</code></pre>
     hl.highlightElement(el as any)
     return el
