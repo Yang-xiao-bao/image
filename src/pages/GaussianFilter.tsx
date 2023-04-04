950c007fbfd4bfbac0ebd2c417b539e7162f7b08
YY <yongfan98@gmail.com>
2023-04-04 21:52:14 +0800

    add gaussian filter

---
+++ src/pages/GaussianFilter.tsx
@@ -0,0 +1,70 @@
+import { Input, InputGroup, InputLeftAddon, Progress, ProgressIndicator } from "@hope-ui/solid";
+import { createMemoObject } from "@solidjs/router/dist/utils";
+import { createEffect, createMemo, createSignal } from "solid-js";
+import { ImagePreview } from "../components/ImagePreview";
+import { ImageSelector } from "../components/ImageSelector";
+import { useAsyncConvolve } from "../hooks/useAsyncConvolve";
+import { useSeparableConvolve } from "../hooks/useSeparableConvolve";
+import { gaussianFilter } from '../libs/gaussian'
+import style from './BoxFilter.module.css'
+
+export function GaussianFilter() {
+  const [size, setSize] = createSignal(20)
+  const [img, setImage] = createSignal<ImageData>()
+  const [result, progress, process] = useSeparableConvolve()
+  const kernel = createMemo(() => {
+    return gaussianFilter(size() / 6)
+  })
+  createEffect(() => {
+    const image = img()
+    if (image) {
+      process(image, kernel())
+    }
+  })
+  const progressBar = createMemo(() => {
+    const p = progress()
+    if (p > 0 && p < 1) {
+      return <Progress value={p * 100} >
+        <ProgressIndicator />
+      </Progress>
+    }
+    return null
+  })
+
+  return <div>
+    <ImageSelector
+      onSelect={setImage}
+    />
+    <InputGroup>
+      <InputLeftAddon>Size</InputLeftAddon>
+      <Input
+        type="number"
+        min={3}
+        step={1}
+        value={size()}
+        onChange={e => {
+          setSize(e.currentTarget.valueAsNumber)
+        }}
+      />
+    </InputGroup>
+    <div class={style.container}>
+      {createMemo(() => {
+        const image = img()
+        if (image) {
+          return <ImagePreview image={image} />
+        }
+        return null
+      })}
+      {
+        createMemo(() => {
+          const image = result()
+          if (image) {
+            return <ImagePreview image={image} />
+          }
+          return null
+        })
+      }
+    </div>
+    {progressBar}
+  </div>
+}
