c982fd17803a46b70ccdba22694604ca96b2e48c
YY <yongfan98@gmail.com>
2023-05-04 21:56:25 +0800

    show pixel value

--- src/components/ImagePreview.tsx
+++ src/components/ImagePreview.tsx
@@ -1,5 +1,5 @@
 import { Text } from "@hope-ui/solid"
-import { createEffect } from "solid-js"
+import { createEffect, createMemo, createSignal } from "solid-js"
 import { FloatImageData, toImageData } from "../libs/image"
 import { show } from "../libs/show"
 import { Channel } from "../types/image"
@@ -13,21 +13,54 @@ export type ImagePreviewProps = {
   title?: string
 }
 export function ImagePreview(props: ImagePreviewProps) {
+  const [pos, setPos] = createSignal<[number, number] | null>()
   const canvas = <canvas
-    class={props.class+ ' ' + style.canvas}
+    class={props.class + ' ' + style.canvas}
+    onMouseMove={e => {
+      const x = e.offsetX / e.currentTarget.clientWidth
+      const y = e.offsetY / e.currentTarget.clientHeight
+      setPos([x, y])
+    }}
+    onMouseOut={e => setPos(null)}
     width={props.image.width}
     height={props.image.height}
   /> as HTMLCanvasElement
   createEffect(() => {
     const ctx = canvas.getContext('2d')
-    const imageData = props.image instanceof ImageData 
-      ? props.image 
+    const imageData = props.image instanceof ImageData
+      ? props.image
       : toImageData(props.image)
     show(imageData, ctx!, props.channel ?? 'all')
   })
+  const rgb = createMemo(() => {
+    const p = pos()
+    if (p) {
+      const x = ~~(p[0] * (props.image.width - 1))
+      const y = ~~(p[1] * (props.image.height - 1))
+      const r = props.image.data[4 * (x + y * props.image.width)]
+      const g = props.image.data[4 * (x + y * props.image.width) + 1]
+      const b = props.image.data[4 * (x + y * props.image.width) + 2]
+      return `rgb(${r},${g},${b})`
+    }
+    return null
+  })
+  const rgbStyle = createMemo(() => {
+    const p = pos()
+    if (p) {
+      if (p[0] > 0.5 && p[1] > 0.5) {
+        return {
+          left: "0"
+        }
+      }
+    }
+    return { right: "0" }
+  })
 
-  return <div>
+  return <div class={style.container}>
     <Text>{props.title}</Text>
     {canvas}
+    <div class={style.rgb} style={rgbStyle()}>
+      {rgb}
+    </div>
   </div>
 }
