0f4992549c4e539eb74b0adebaf6326695ce3955
YY <yongfan98@gmail.com>
2023-09-14 14:30:39 +0800

    erode

---
+++ src/libs/morphological/erode.ts
@@ -0,0 +1,30 @@
+import { BinaryImageData } from "../image";
+import { StructureElements } from "./se";
+
+export function erode(img: BinaryImageData, se: StructureElements) {
+  const r: BinaryImageData = {
+    width: img.width,
+    height: img.height,
+    data: new Uint8Array(img.data.length)
+  }
+  for (let x = se.ox; x < (img.width - (se.width - se.ox)); x++) {
+    for (let y = se.oy; y < (img.height - (se.height - se.oy)); y++) {
+      let contained = true
+      for (let s = 0; s < se.width; s++) {
+        for (let t = 0; t < se.height; t++) {
+          if (se.data[s + t * se.width] === 1 &&
+            img.data[(x + s) + (y + t) * img.width] === 0
+          ) {
+            contained = false
+            break
+          }
+        }
+      }
+      if(contained) {
+        r.data[x + y * r.width] = 1
+      }
+    }
+  }
+  console.log(r)
+  return r
+}
---
+++ src/libs/morphological/se.ts
@@ -0,0 +1,7 @@
+export type StructureElements = {
+  width: number,
+  height: number,
+  ox: number,
+  oy: number,
+  data: Uint8Array
+}
